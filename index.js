f4b05748c37673c509be06228460f9645d72dd29
Lim Yee Han <limyeehan@gmail.com>
2024-02-01 00:59:34 +0800

    logging

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 // 1. Amount to transfer for each transaction, in Ether
 const TRANSFER_SIZE = "0.0000005"; // $0.00115 USD
 // 2. Number of transactions to send
-const TX_COUNT = 10;
+const TX_COUNT = 3;
 
 const PK1 = process.env.PRIVATE_KEY_1;
 const PK2 = process.env.PRIVATE_KEY_2;
@@ -19,9 +19,11 @@ const Wallet2 = new ethers.Wallet(PK2, provider);
 
 // Convert Ether value to Wei (1 Ether = 10^18 Wei)
 const valueInEtherToTransfer = TRANSFER_SIZE;
-const valueInWei = ethers.utils.parseEther(valueInEtherToTransfer.toString());
+const valueInWeiToTransfer = ethers.utils.parseEther(
+  valueInEtherToTransfer.toString()
+);
 
-const transfer = async (source, destination, valueInWei) => {
+const transfer = async (i, source, destination, valueInWei) => {
   // Gas in wei
   const gasPriceInWei = await provider.getGasPrice();
 
@@ -34,35 +36,56 @@ const transfer = async (source, destination, valueInWei) => {
 
   try {
     const txResponse = await source.sendTransaction(tx);
-    console.log("Transaction hash:", txResponse.hash);
+    console.log(`${i}) Transaction hash: ${txResponse.hash}`);
   } catch (error) {
-    // console.error("Error:", error);
-  }
-};
-
-const eth_getBalance = async (i) => {
-  try {
-    const balance1 = await provider.getBalance(Wallet1.address);
-    const balance2 = await provider.getBalance(Wallet2.address);
-    console.log(`${i}. Wallet1 balance: ${ethers.utils.formatEther(balance1)}`);
-  } catch (error) {
-    // console.log(error);
+    console.error("Error:", error);
   }
 };
 
 const run = async () => {
-  // Repeat 10 times:
+  // Get initial balances
+  const initial_balance1 = await provider.getBalance(Wallet1.address);
+  const initial_balance2 = await provider.getBalance(Wallet2.address);
+  console.log(
+    `Wallet1 initial balance: ${ethers.utils.formatEther(initial_balance1)}\n` +
+      `Wallet2 initial balance: ${ethers.utils.formatEther(initial_balance2)}\n`
+  );
+
+  let total_success = 0;
+
+  // Repeat TX_COUNT times:
   for (let i = 0; i < TX_COUNT; i++) {
     try {
-      // Get ETH initial balance of Wallet1 and Wallet2
-      await eth_getBalance(i);
-
       // Transfer ETH from Wallet1 to Wallet2
-      await transfer(Wallet1, Wallet2, valueInWei);
+      await transfer(i, Wallet1, Wallet2, valueInWeiToTransfer);
     } catch (error) {
       continue;
     }
+
+    total_success += 1;
   }
+
+  // Get final balances
+  const final_balance1 = await provider.getBalance(Wallet1.address);
+  const final_balance2 = await provider.getBalance(Wallet2.address);
+  console.log(
+    `\nWallet1 final balance: ${ethers.utils.formatEther(final_balance1)}\n` +
+      `Wallet2 final balance: ${ethers.utils.formatEther(final_balance2)}`
+  );
+  const total_out = initial_balance1.sub(final_balance1);
+  const total_transfer_out = valueInWeiToTransfer.mul(
+    ethers.BigNumber.from(total_success)
+  );
+  const total_gas_expenditure = total_out.sub(total_transfer_out);
+  console.log(
+    `\nTotal Successful Transfers: ${total_success}\n` +
+      `Total Transfer Value: ${ethers.utils.formatEther(
+        total_transfer_out
+      )} ETH\n` +
+      `Total Gas Expenditure: ${ethers.utils.formatEther(
+        total_gas_expenditure
+      )} ETH = $${Number(ethers.utils.formatEther(total_out)) * 2330}`
+  );
 };
 
 run();
